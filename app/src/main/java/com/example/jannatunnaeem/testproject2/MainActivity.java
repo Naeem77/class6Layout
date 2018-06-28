a169115dd3c98c4185287955cb74980297851298
Jannatun Naeem <jannatnnaeem@gmail.com>
2018-06-28 04:40:43 -0700

    initial commit added.

--- app/src/main/java/com/example/jannatunnaeem/testproject2/MainActivity.java
+++ app/src/main/java/com/example/jannatunnaeem/testproject2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jannatunnaeem.testproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoSecondActiviy(View view) {
+        Intent intent = new Intent(MainActivity.this, secondActivity.class);
+        startActivity(intent);
     }
 }
